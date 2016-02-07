b840ba829f9e55c2e31c847cedfd6cbad99f3469
L. Mountrakis <LMountr@gmail.com>
2016-02-08 01:04:50 +0200

    Restructured parts of the code to fit pep8 and pylint

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Description :
+              Kloeden - Numerical Solution of stochastic differential
+              equations (Springer 1992)  page XXX.
+              Strong order 1.0 Runge Kutta scheme
+Author      : Lampros Mountrakis
+Date        :  2010/04/25
+"""
+__author__ = 'Lampros Mountrakis'
+__email__ = 'github_blame@lmount.eu'
+__version__ = '0.1b'
